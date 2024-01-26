a7302dc7a09d85ffcd7f250acdf3f85750db4413
migromarj <miguelromeroarjona@gmail.com>
2024-01-26 18:25:28 +0100

    Add configuration to use BOTICA system shutdown process

---
+++ src/main/java/com/template/main/LaunchShutdown.java
@@ -0,0 +1,35 @@
+package com.template.main;
+
+import java.util.List;
+
+import com.botica.runners.CollectorLoader;
+import com.botica.runners.ShutdownLoader;
+import com.botica.utils.shutdown.ShutdownUtils;
+
+public class LaunchShutdown {
+
+    private static String shutdownPropertiesFilePath = "src/main/resources/BOTICAConfig/shutdown.properties";
+    private static String configurationPropertiesFilePath = "src/main/resources/BOTICAConfig/collector.properties";
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            shutdownPropertiesFilePath = args[0];
+        } 
+
+        if (args.length > 1) {
+            configurationPropertiesFilePath = args[1];
+        }
+
+        ShutdownLoader shutdownLoader = new ShutdownLoader(shutdownPropertiesFilePath, true);
+        CollectorLoader collectorLoader = new CollectorLoader(configurationPropertiesFilePath, true);
+
+        List<String> botBotsToShutDown = shutdownLoader.getBotsToShutDown();
+        String host = shutdownLoader.getHost();
+        String shutdownCommandType = shutdownLoader.getShutdownCommandType();
+        Integer timeToWait = shutdownLoader.getTimeToWait();
+        String shutdownQueue = shutdownLoader.getShutdownQueue();
+
+        ShutdownUtils.shutdown(botBotsToShutDown, shutdownCommandType, timeToWait, shutdownQueue, host, collectorLoader);
+    }   
+}
